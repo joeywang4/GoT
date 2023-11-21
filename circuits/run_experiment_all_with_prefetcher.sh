c12b97295ac526be6996bd59bf9913bdfff9ce6d
Ping-Lun Wang <b05901143@ntu.edu.tw>
2023-11-21 14:58:52 -0500

    Update run_experiment_all_with_prefetcher.sh

--- circuits/run_experiment_all_with_prefetcher.sh
+++ circuits/run_experiment_all_with_prefetcher.sh
@@ -62,93 +62,93 @@ cat $1/ALU_MAJ/RESULT/final.csv | datamash mean 1 > $1/ALU_MAJ/RESULT/FINAL_MEAN
 
 
 
-mkdir -pv $1/GOL_GLIDER_1
+# mkdir -pv $1/GOL_GLIDER_1
 
-./run_experiment_pipe_auto.sh $1/GOL_GLIDER_1 0 gol_glider 1 200
+# ./run_experiment_pipe_auto.sh $1/GOL_GLIDER_1 0 gol_glider 1 200
 
-mkdir -pv $1/GOL_GLIDER_1/RESULT
+# mkdir -pv $1/GOL_GLIDER_1/RESULT
 
-cat $1/GOL_GLIDER_1/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_1/RESULT/all.csv
-cat $1/GOL_GLIDER_1/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_1/RESULT/ALL_MEDIAN
-cat $1/GOL_GLIDER_1/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_1/RESULT/ALL_MEAN
+# cat $1/GOL_GLIDER_1/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_1/RESULT/all.csv
+# cat $1/GOL_GLIDER_1/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_1/RESULT/ALL_MEDIAN
+# cat $1/GOL_GLIDER_1/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_1/RESULT/ALL_MEAN
 
-cat $1/GOL_GLIDER_1/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_1/RESULT/final.csv
-cat $1/GOL_GLIDER_1/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_1/RESULT/FINAL_MEDIAN
-cat $1/GOL_GLIDER_1/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_1/RESULT/FINAL_MEAN
-
-
-mkdir -pv $1/GOL_GLIDER_10
-
-./run_experiment_pipe_auto.sh $1/GOL_GLIDER_10 0 gol_glider 10 200
-
-mkdir -pv $1/GOL_GLIDER_10/RESULT
-
-cat $1/GOL_GLIDER_10/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_10/RESULT/all.csv
-cat $1/GOL_GLIDER_10/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_10/RESULT/ALL_MEDIAN
-cat $1/GOL_GLIDER_10/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_10/RESULT/ALL_MEAN
-
-cat $1/GOL_GLIDER_10/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_10/RESULT/final.csv
-cat $1/GOL_GLIDER_10/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_10/RESULT/FINAL_MEDIAN
-cat $1/GOL_GLIDER_10/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_10/RESULT/FINAL_MEAN
+# cat $1/GOL_GLIDER_1/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_1/RESULT/final.csv
+# cat $1/GOL_GLIDER_1/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_1/RESULT/FINAL_MEDIAN
+# cat $1/GOL_GLIDER_1/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_1/RESULT/FINAL_MEAN
 
 
-mkdir -pv $1/GOL_GLIDER_20
+# mkdir -pv $1/GOL_GLIDER_10
 
-./run_experiment_pipe_auto.sh $1/GOL_GLIDER_20 0 gol_glider 20 200
+# ./run_experiment_pipe_auto.sh $1/GOL_GLIDER_10 0 gol_glider 10 200
 
-mkdir -pv $1/GOL_GLIDER_20/RESULT
+# mkdir -pv $1/GOL_GLIDER_10/RESULT
 
-cat $1/GOL_GLIDER_20/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_20/RESULT/all.csv
-cat $1/GOL_GLIDER_20/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_20/RESULT/ALL_MEDIAN
-cat $1/GOL_GLIDER_20/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_20/RESULT/ALL_MEAN
+# cat $1/GOL_GLIDER_10/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_10/RESULT/all.csv
+# cat $1/GOL_GLIDER_10/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_10/RESULT/ALL_MEDIAN
+# cat $1/GOL_GLIDER_10/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_10/RESULT/ALL_MEAN
 
-cat $1/GOL_GLIDER_20/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_20/RESULT/final.csv
-cat $1/GOL_GLIDER_20/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_20/RESULT/FINAL_MEDIAN
-cat $1/GOL_GLIDER_20/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_20/RESULT/FINAL_MEAN
+# cat $1/GOL_GLIDER_10/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_10/RESULT/final.csv
+# cat $1/GOL_GLIDER_10/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_10/RESULT/FINAL_MEDIAN
+# cat $1/GOL_GLIDER_10/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_10/RESULT/FINAL_MEAN
 
 
+# mkdir -pv $1/GOL_GLIDER_20
 
-mkdir -pv $1/GOL_GLIDER_30
+# ./run_experiment_pipe_auto.sh $1/GOL_GLIDER_20 0 gol_glider 20 200
 
-./run_experiment_pipe_auto.sh $1/GOL_GLIDER_30 0 gol_glider 30 200
+# mkdir -pv $1/GOL_GLIDER_20/RESULT
 
-mkdir -pv $1/GOL_GLIDER_30/RESULT
+# cat $1/GOL_GLIDER_20/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_20/RESULT/all.csv
+# cat $1/GOL_GLIDER_20/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_20/RESULT/ALL_MEDIAN
+# cat $1/GOL_GLIDER_20/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_20/RESULT/ALL_MEAN
 
-cat $1/GOL_GLIDER_30/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_30/RESULT/all.csv
-cat $1/GOL_GLIDER_30/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_30/RESULT/ALL_MEDIAN
-cat $1/GOL_GLIDER_30/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_30/RESULT/ALL_MEAN
-
-cat $1/GOL_GLIDER_30/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_30/RESULT/final.csv
-cat $1/GOL_GLIDER_30/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_30/RESULT/FINAL_MEDIAN
-cat $1/GOL_GLIDER_30/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_30/RESULT/FINAL_MEAN
+# cat $1/GOL_GLIDER_20/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_20/RESULT/final.csv
+# cat $1/GOL_GLIDER_20/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_20/RESULT/FINAL_MEDIAN
+# cat $1/GOL_GLIDER_20/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_20/RESULT/FINAL_MEAN
 
 
 
-mkdir -pv $1/GOL_GLIDER_40
+# mkdir -pv $1/GOL_GLIDER_30
 
-./run_experiment_pipe_auto.sh $1/GOL_GLIDER_40 0 gol_glider 40 200
+# ./run_experiment_pipe_auto.sh $1/GOL_GLIDER_30 0 gol_glider 30 200
 
-mkdir -pv $1/GOL_GLIDER_40/RESULT
+# mkdir -pv $1/GOL_GLIDER_30/RESULT
 
-cat $1/GOL_GLIDER_40/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_40/RESULT/all.csv
-cat $1/GOL_GLIDER_40/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_40/RESULT/ALL_MEDIAN
-cat $1/GOL_GLIDER_40/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_40/RESULT/ALL_MEAN
+# cat $1/GOL_GLIDER_30/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_30/RESULT/all.csv
+# cat $1/GOL_GLIDER_30/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_30/RESULT/ALL_MEDIAN
+# cat $1/GOL_GLIDER_30/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_30/RESULT/ALL_MEAN
 
-cat $1/GOL_GLIDER_40/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_40/RESULT/final.csv
-cat $1/GOL_GLIDER_40/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_40/RESULT/FINAL_MEDIAN
-cat $1/GOL_GLIDER_40/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_40/RESULT/FINAL_MEAN
+# cat $1/GOL_GLIDER_30/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_30/RESULT/final.csv
+# cat $1/GOL_GLIDER_30/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_30/RESULT/FINAL_MEDIAN
+# cat $1/GOL_GLIDER_30/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_30/RESULT/FINAL_MEAN
 
 
-mkdir -pv $1/GOL_GLIDER_50
 
-./run_experiment_pipe_auto.sh $1/GOL_GLIDER_50 0 gol_glider 50 200
+# mkdir -pv $1/GOL_GLIDER_40
 
-mkdir -pv $1/GOL_GLIDER_50/RESULT
+# ./run_experiment_pipe_auto.sh $1/GOL_GLIDER_40 0 gol_glider 40 200
 
-cat $1/GOL_GLIDER_50/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_50/RESULT/all.csv
-cat $1/GOL_GLIDER_50/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_50/RESULT/ALL_MEDIAN
-cat $1/GOL_GLIDER_50/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_50/RESULT/ALL_MEAN
+# mkdir -pv $1/GOL_GLIDER_40/RESULT
 
-cat $1/GOL_GLIDER_50/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_50/RESULT/final.csv
-cat $1/GOL_GLIDER_50/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_50/RESULT/FINAL_MEDIAN
-cat $1/GOL_GLIDER_50/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_50/RESULT/FINAL_MEAN
+# cat $1/GOL_GLIDER_40/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_40/RESULT/all.csv
+# cat $1/GOL_GLIDER_40/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_40/RESULT/ALL_MEDIAN
+# cat $1/GOL_GLIDER_40/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_40/RESULT/ALL_MEAN
+
+# cat $1/GOL_GLIDER_40/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_40/RESULT/final.csv
+# cat $1/GOL_GLIDER_40/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_40/RESULT/FINAL_MEDIAN
+# cat $1/GOL_GLIDER_40/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_40/RESULT/FINAL_MEAN
+
+
+# mkdir -pv $1/GOL_GLIDER_50
+
+# ./run_experiment_pipe_auto.sh $1/GOL_GLIDER_50 0 gol_glider 50 200
+
+# mkdir -pv $1/GOL_GLIDER_50/RESULT
+
+# cat $1/GOL_GLIDER_50/* | grep "Whole.*" | awk -F":" '{print $3}' > $1/GOL_GLIDER_50/RESULT/all.csv
+# cat $1/GOL_GLIDER_50/RESULT/all.csv | datamash median 1 > $1/GOL_GLIDER_50/RESULT/ALL_MEDIAN
+# cat $1/GOL_GLIDER_50/RESULT/all.csv | datamash mean 1 > $1/GOL_GLIDER_50/RESULT/ALL_MEAN
+
+# cat $1/GOL_GLIDER_50/* | grep "Whole.*" | awk -F":" '{print $3}' | shuf -n 500 > $1/GOL_GLIDER_50/RESULT/final.csv
+# cat $1/GOL_GLIDER_50/RESULT/final.csv | datamash median 1 > $1/GOL_GLIDER_50/RESULT/FINAL_MEDIAN
+# cat $1/GOL_GLIDER_50/RESULT/final.csv | datamash mean 1 > $1/GOL_GLIDER_50/RESULT/FINAL_MEAN
